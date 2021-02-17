02bfb46bf4d19117b6ba0724002343e39d2e6246
Sagar Shah <sa64r.shah@gmail.com>
2021-02-17 16:53:33 +0000

    added authorised files and non-authorised files

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const { ppid } = require("process");
 const ejsMate = require('ejs-mate')
 const mongoose = require('mongoose');
 const Item = require('./models/item');
+const methodOverride = require('method-override');
 
 
 
@@ -32,19 +33,24 @@ app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views')) //to ensure ejs views is found from being run anywhere out of the folder of index.js
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'));
 
+app.get('/login', (req, res) => {
+    res.render('pages/login')
+});
 
-app.get('/', (req, res) => {
-    res.render('pages/home')
+app.get('/', async (req, res) => {
+    const items = await Item.find({});
+    res.render('authorised-pages/home', { items })
 });
 
 app.get('/items', async (req, res) => {
     const items = await Item.find({});
-    res.render('pages/list', { items })
+    res.render('authorised-pages/list', { items })
 });
 
 app.get('/items/new', (req, res) => {
-    res.render('pages/new')
+    res.render('authorised-pages/new')
 });
 
 app.post('/items', async (req, res) => {
@@ -55,16 +61,17 @@ app.post('/items', async (req, res) => {
 
 app.get('/items/:id', async (req, res) => {
     const item = await Item.findById(req.params.id)
-    res.render('pages/item', { item })
+    res.render('authorised-pages/item', { item })
 });
 
-app.get('/items/:id/edit', (req, res) => {
-    res.send("Edit Details About Item")
+app.get('/items/:id/edit', async (req, res) => {
+    const item = await Item.findById(req.params.id)
+    res.render('authorised-pages/edit', { item })
 });
 
 // routes are matched in order, so * has to be last
 app.get('*', (req, res) => {
-    res.render('pages/page-not-found')
+    res.render('authorised-pages/page-not-found')
 });
 
 app.listen(3000, () => {
